0a27cab7f0530a18b0486e49853d0614aabd6dca
Rahul Rajish Nathan <74354544+rahulrajish@users.noreply.github.com>
2023-08-19 11:50:10 +0530

    Update main.rs
    Removing unwrap and adding error messages

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,47 @@ use std::{
 use web_server_rust::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    if let Ok(listener) = TcpListener::bind("127.0.0.1:7878") {
+        let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
+        for stream in listener.incoming().take(2) {
+        //let stream = stream.unwrap();
 
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+            if let Ok(ok_stream) = stream {
+                pool.execute(|| {
+                    handle_connection(ok_stream);
+                });
+            }
+        }
+
+        println!("Shutting down.");
     }
-
-    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
+    //let request_line = buf_reader.lines().next().unwrap().unwrap();
+    if let Some(request) = buf_reader.lines().next() {
+        match request {
+            Ok(request_line) => {
+                let (status_line, filename) = match &request_line[..] {
+                    "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+                    "GET /sleep HTTP/1.1" => {
+                        thread::sleep(Duration::from_secs(5));
+                        ("HTTP/1.1 200 OK", "hello.html")
+                    }
+                    _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+                };
+                    
+                if let Ok(contents) = fs::read_to_string(filename) {
+                    let length = contents.len();
+            
+                    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
+            },
+            Err(_) => println!("Error while handling connections"),
         }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
-        
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
 }
